f96c19e9c11f39c9774315ab45927fbccea40101
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-22 19:39:10 +0530

    Generated: project-root/src/models/song.js

---
+++ project-root/src/models/song.js
@@ -0,0 +1,10 @@
+class Song {
+  constructor(title, artist, album, downloadURL) {
+    this.title = title;
+    this.artist = artist;
+    this.album = album;
+    this.downloadURL = downloadURL;
+  }
+}
+
+module.exports = Song;
